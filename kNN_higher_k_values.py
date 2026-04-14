25686b60bafc3dcd2e890f37757dfec0bda0aedf
Kaushik Acharya <kaushik@palebluedot.net>
2026-04-13 17:38:35 -0700

    Higer K regression and cleanup

---
+++ kNN_higher_k_values.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import train_test_split
+
+def find_nearest(array,value):
+    
+    idx = pd.Series(np.abs(array - value)).idxmin()
+
+    # Return the nearest neighbor index and value
+    return idx, array[idx]
+
+df_adv = pd.read_csv('data/Advertising.csv')
+
+# Use the TV column as the predictor
+x = df_adv[['TV']].values
+
+# Use the Sales column as the response
+y = df_adv[['Sales']].values
+
+# Split the dataset in training and testing with 60% training set 
+# and 40% testing set with random state = 42
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.6,random_state=42)
+
+
+# Choose the minimum k value based on the instructions given on the left
+k_value_min = 1
+
+# Choose the maximum k value based on the instructions given on the left
+k_value_max = 70
+
+# Create a list of integer k values betwwen k_value_min and k_value_max using linspace
+# Produces an array of ints from 1 through 70
+k_list = np.linspace(k_value_min, k_value_max, 70)
+
+# Set the grid to plot the values
+fig, ax = plt.subplots(figsize=(10,6))
+
+# Variable used to alter the linewidth of each plot
+j=0
+
+# Loop over all the k values (array of 1 through 70)
+for k_value in k_list:   
+    
+    # Creating a kNN Regression model 
+    model = KNeighborsRegressor(n_neighbors=int(k_value))
+    
+    # Fitting the regression model on the training data 
+    model.fit(x_train,y_train)
+    
+    # Use the trained model to predict on the test data 
+    y_pred = model.predict(x_train)
+    
+    # Helper code to plot the data along with the model predictions
+    colors = ['grey','r','b']
+    if k_value in [1,10,70]:
+        xvals = np.linspace(x.min(),x.max(),100).reshape(-1,1)
+        ypreds = model.predict(xvals)
+        ax.plot(xvals, ypreds,'-',label = f'k = {int(k_value)}',linewidth=j+2,color = colors[j])
+        j+=1
+        
+ax.legend(loc='lower right',fontsize=20)
+# ax.plot(x_train, y_train,'x',label='train',color='k')
+ax.plot(x_test, y_test,'x',label='train',color='k')
+ax.set_xlabel('TV budget in $1000',fontsize=20)
+ax.set_ylabel('Sales in $1000',fontsize=20)
+plt.tight_layout()
+plt.show()
--- kNN_regression.py
+++ kNN_regression.py
@@ -8,41 +8,32 @@ from sklearn.model_selection import train_test_split
 
 def find_nearest(array,value):
     
-    # print("\nInput array:  ", array, "Input value: ", value, "\n\n")
-    # Hint: To find idx, use .idxmin() function on the series
     idx = pd.Series(np.abs(array - value)).idxmin()
 
     # Return the nearest neighbor index and value
     return idx, array[idx]
 
 df_adv = pd.read_csv('data/Advertising.csv')
-# print(df_adv.head())
 
 # Get a subset of the data, e.g., rows 5 to 13
 # Use the TV column as the predictor
 x_true = df_adv.TV.iloc[5:13]
-print(x_true)
 
 # Use the Sales column as the response
 y_true = df_adv.Sales.iloc[5:13]
-print(y_true)
 
 # Sort the data to get indices ordered from lowest to highest TV values
 idx = np.argsort(x_true).values
-# print(idx)
 
 # Get the predictor data in the order given by idx above
 x_true  = x_true.iloc[idx].values
-print("x_true (TV budget)\n", x_true, "\n\n")
 
 # Get the response data in the order given by idx above
 y_true  = y_true.iloc[idx].values
-print("y_true(Sales)\n", y_true, "\n\n")
 
 # Create some synthetic x-values (might not be in the actual dataset)
 # numpy.linspace() returns evenly spaced values over a specified interval
 x = np.linspace(np.min(x_true), np.max(x_true))
-# print("Output of linspace on x_true:", x, "\n\n")
 
 # Initialize the y-values for the length of the synthetic x-values to zero
 y = np.zeros((len(x)))
